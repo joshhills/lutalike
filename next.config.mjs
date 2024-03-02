b0b5eaa6d14a86d20078a11e40df74d3d990e373
Josh Hills <joshuamarcushills@gmail.com>
2024-03-02 18:52:16 +0000

    chore(next.config.mjs): disable image optimization

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: "export",
-    basePath: "/lutalike"
+    basePath: "/lutalike",
+    images: {
+        unoptimized: true,
+    }
 };
 
 export default nextConfig;
