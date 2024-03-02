0806b78e33c9b2abb7339998b59a213ff1cfa53d
Josh Hills <joshuamarcushills@gmail.com>
2024-03-02 23:05:46 +0000

    feat: parse metadata of example .dng file

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,54 @@
+"use client";
+
 import Header from "./ui/header";
+import exifr from "exifr";
+import http from "http";
+import Image from "next/image";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const [metadata, setMetadata] = useState();
+
+  useEffect(() => {
+    async function getBufferFromUrl(url: string): Promise<Buffer> {
+      return new Promise((resolve) => {
+        http.get(url, (response) => {
+          const body: Buffer[] = [];
+          response
+            .on("data", (chunk: Buffer) => {
+              body.push(chunk);
+            })
+            .on("end", () => {
+              resolve(Buffer.concat(body));
+            });
+        });
+      });
+    }
+
+    const test = async () => {
+      const buffer = await getBufferFromUrl(
+        "http://localhost:3000/lutalike/example-raw.dng",
+      );
+
+      exifr.parse(buffer, { tiff: false, xmp: true }).then((output: any) => {
+        setMetadata(output);
+      });
+    };
+
+    test();
+  }, [metadata]);
+
   return (
     <>
       <Header />
-      <main className="flex min-h-screen flex-col items-center justify-between p-24">
-        Hello
+      <main>
+        <Image
+          width="600"
+          height="600"
+          src="/lutalike/example-after.jpg"
+          alt=""
+        />
+        <pre>{JSON.stringify(metadata, null, 2)}</pre>
       </main>
     </>
   );
